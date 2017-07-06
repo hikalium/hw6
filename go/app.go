b1c1e8a5e10fef26bd8f856b2f9e0ab135039d0a
hikalium <hikalium@hikalium.com>
2017-07-07 02:55:02 +0900

    Impl パタトクカシーー

--- go/app.go
+++ go/app.go
@@ -10,6 +10,42 @@ func init() {
 }
 
 func handlePata(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Fprintln(w, r.Form)
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "Hello world!\n")
+	a := r.Form.Get("a")
+	b := r.Form.Get("b")
+	fmt.Fprintln(w, a)
+	fmt.Fprintln(w, b)
+	ans := generatePatatokakushi(a, b)
+	fmt.Fprintln(w, ans)
+}
+
+// https://mrekucci.blogspot.jp/2015/07/dont-abuse-mathmax-mathmin.html
+func Max(x, y int) int {
+    if x > y {
+        return x
+    }
+    return y
+}
+
+func generatePatatokakushi(a, b string) string {
+/*
+	if a == nil || b == nil {
+		return ""
+	}
+*/
+	var ans string;
+	ra := []rune(a)
+	rb := []rune(b)
+	maxLen := Max(len(ra), len(rb))
+	for i := 0; i < maxLen; i++ {
+        if i < len(ra) {
+			ans += string(ra[i])
+		}
+        if i < len(rb) {
+			ans += string(rb[i])
+		}
+    }
+	return ans
 }
